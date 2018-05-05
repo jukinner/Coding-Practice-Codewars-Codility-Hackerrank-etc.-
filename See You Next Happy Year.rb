eb69d59703c4b3f9384a89513010cf1c2b3cf1eb
Jukinner <jukinner@gmail.com>
2018-05-05 12:20:37 -0600

    See you next happy Year-simple Code Wars

---
+++ See You Next Happy Year.rb
@@ -0,0 +1,33 @@
+# See You Next Happy Year
+# Scenario
+# You're saying good-bye your best friend , See you next happy year .
+
+# Happy Year is the year with only distinct digits , (e.g) 2018
+
+# Task
+# Given a year, Find The next happy year or The closest year You'll see your best friend  !alt !alt
+
+# Notes
+# Year Of Course always Positive .
+# Have no fear , It is guaranteed that the answer exists .
+# It's not necessary that the year passed to the function is Happy one .
+# Input Year with in range (1000  ≤  y  ≤  9000)
+# Input >> Output Examples:
+# 1- nextHappyYear (7712) ==> return (7801)
+
+
+def next_happy_year(year)
+  x = false
+  year = year + 1
+  while x == false
+    if year.digits.uniq.count == 4
+      p year
+      x = true
+    else 
+      year = year + 1
+    end
+  end
+end
+
+next_happy_year(1001)
+

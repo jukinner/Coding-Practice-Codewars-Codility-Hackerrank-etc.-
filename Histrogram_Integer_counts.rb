56604cd76f8e8773b86434057dd3cf7dc203e45b
Jukinner <jukinner@gmail.com>
2018-05-09 18:14:31 -0600

    Historgram Integer count, Ruby with alternate method

--- Histrogram_Integer_counts.rb
+++ Histrogram_Integer_counts.rb
@@ -6,7 +6,16 @@ def num_counter(arr)
   return answer
 end
 # arr = [1, 4, 1, 3, 2, 1, 4, 5, 4, 4, 5, 4]
-# num_counter(arr)
+# num_counter(arr)  
+
+# Jordan's answer
+# Whenever using inject and passing it a block, the first value is the incrementer value(hash in this case), next one is the element we are iterating over). the first thing when adding to a hash is to call it. in this case, all we are doing is iterating the value count of the the key, by 1, then you HAVE to return the hash at teh end of the block so the hash method is not reset. 
+def num_counter(arr)
+  arr.inject(Hash.new(0)) do |hash, e|
+    hash[e] += 1
+    hash
+  end
+end
 
 describe 'Number counter' do
   it 'returns a hash that contains the number of occurrences for each array element' do

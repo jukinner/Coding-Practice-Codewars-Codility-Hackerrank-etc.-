1f7d1ade60b51c1ab964f22dd071c4572b0af7ac
Jukinner <jukinner@gmail.com>
2018-06-14 08:48:04 -0600

    Finished Prime Factor-2 options

--- Prime_Factor_Decomposition.rb
+++ Prime_Factor_Decomposition.rb
@@ -6,21 +6,45 @@ def primeFactors(n)
   divisors = []
   i = 2
   while Prime.prime?(d) == false
-    if Prime.prime?(i) && d % i == 0
+    divisors = []
+    if Prime.prime?(i)
       while d % i == 0
-      
-#       if d % i == 0
         d = d / i
         p divisors << i
       end
     p factors << divisors
-    i += 1
-    divisors = []
+    end
+  i += 1
+  end
+  factors << [d]
+  formatted = []
+  factors.each do |x|
+    if x.count > 1
+      formatted << "(#{x[0]}**#{x.count})"
+    else
+      formatted << "(#{x[0]})"
     end
   end
-  p factors
-  p d
-  p i
+  p formatted.join
 end
 
 primeFactors(7775460)
+# (2**2)(3**3)(5)(7)(11**2)(17)
+
+
+# Simpler way using built in Prime division library
+def primeFactors(n)
+  answer = Prime.prime_division(n)
+  formatted = []
+  answer.each do |x|
+    if x[1] > 1
+      formatted << "(#{x[0]}**#{x[1]})"
+    elsif x[1] == 1
+      formatted << "(#{x[0]})"
+    end
+  end
+  p formatted.join
+end
+
+
+

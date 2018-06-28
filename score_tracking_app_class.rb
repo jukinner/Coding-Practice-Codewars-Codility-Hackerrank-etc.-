22710009d97de1900f12671b01a90271d6dbf206
Jukinner <jukinner@gmail.com>
2018-06-28 09:35:50 -0600

    score tracking-added alternate way to make class

--- score_tracking_app_class.rb
+++ score_tracking_app_class.rb
@@ -44,6 +44,40 @@ class Scoreboard
   end
 end
 
+# alternate way
+# class Scoreboard
+#   attr_reader :score
+
+#   def initialize(team)
+#     @team = team
+#     @score = score_builder
+#   end
+
+#   def score_builder
+#     {
+#       @team => {
+#         1 => 0,
+#         2 => 0,
+#         3 => 0,
+#         4 => 0,
+#         5 => 0,
+#         6 => 0,
+#         7 => 0,
+#         8 => 0,
+#         9 => 0
+#       }
+#     }
+#   end
+
+#   def generate_total
+#     @score[@team].values.inject(&:+)
+#   end
+
+#   def add_run inning:
+#     @score[@team][inning] += 1
+#   end
+# end
+
 describe 'Scoreboard' do
   it 'should properly calculate the total score for a team' do
     team = Scoreboard.new('Astros')

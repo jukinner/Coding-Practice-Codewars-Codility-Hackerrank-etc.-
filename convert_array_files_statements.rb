b7d213a09d5b77edb93195d135efff9944d5df5e
Jukinner <jukinner@gmail.com>
2018-07-02 16:27:35 -0600

    convert array files to .js statements in an array-used map

---
+++ convert_array_files_statements.rb
@@ -0,0 +1,68 @@
+# Convert an Array of File Require Statements into File Names
+# When building scripts, it's a common requirement to remove and update multiple items in a collection of strings. In this guide we're going to mimic the process needed to take a list of require statements and generate a list of file names.
+# Summary
+# Implement a method that generates a list of file names based on a collection of require statements.
+
+# Exercise File
+# Code File
+
+# Exercise Description
+# Build a method that takes in an array of require statements and outputs an array of the file names with their JavaScript extension added.
+
+# Example
+# javascript_files = [
+#   '//= require custom/scripts',
+#   '//= require base/defaults',
+#   '//= require html.sortable',
+#   '//= require moment',
+#   '//= require test-styles'
+# ]
+
+# append_js_file_extension javascript_files
+
+# # Output
+# [
+#   'custom/scripts.js',
+#   'base/defaults.js',
+#   'html.sortable.js',
+#   'moment.js',
+#   'test-styles.js'
+# ]
+
+require 'rspec'
+
+# first pass
+def append_js_file_extension files  
+  p files.map { |e| e.slice!(12..-1) + ".js"}
+end
+
+
+
+describe 'JavaScript file extension' do
+  it 'converts require statements to javascript file name' do
+    javascript_files = [
+      '//= require custom/scripts',
+      '//= require base/defaults',
+      '//= require html.sortable',
+      '//= require moment',
+      '//= require test-styles'
+    ]
+    expect(append_js_file_extension javascript_files).to eq([
+                                                              'custom/scripts.js',
+                                                              'base/defaults.js',
+                                                              'html.sortable.js',
+                                                              'moment.js',
+                                                              'test-styles.js'
+                                                            ])
+  end
+end
+
+
+javascript_files = [
+      '//= require custom/scripts',
+      '//= require base/defaults',
+      '//= require html.sortable',
+      '//= require moment',
+      '//= require test-styles'
+    ]
+append_js_file_extension javascript_files

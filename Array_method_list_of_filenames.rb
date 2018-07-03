e5119d99d9884b9d23f652b6da2769d237a5660c
Jukinner <jukinner@gmail.com>
2018-07-02 19:03:40 -0600

    Updated array method list of filenames

--- Array_method_list_of_filenames.rb
+++ Array_method_list_of_filenames.rb
@@ -19,12 +19,19 @@ class Array
   end
 end
 
-# describe 'File selector' do
-#   it 'returns the ruby file names from an array of file names' do
-#     file_names = %w{file1.rb file2.html file3.rb file4.rb file5.js}
-#     expect(file_names.file_selector 'rb').to eq(['file1', 'file3', 'file4'])
+# Alternate answer
+# class Array
+#   def file_selector ext
+#     grep(/(.*)\.#{ext}/){ $1 }
 #   end
 # end
+
+describe 'File selector' do
+  it 'returns the ruby file names from an array of file names' do
+    file_names = %w{file1.rb file2.html file3.rb file4.rb file5.js}
+    expect(file_names.file_selector 'rb').to eq(['file1', 'file3', 'file4'])
+  end
+end
 
 file_names = %w{file1.rb file2.html file3.rb file4.rb file5.js}
 p file_names.file_selector 'rb'

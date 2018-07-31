e906c77c176b1093e96ba93c5be7300636297062
Jukinner <jukinner@gmail.com>
2018-07-31 08:24:15 -0600

    remove section of url

---
+++ JavaScript_challenges/remove_url_anchor.js
@@ -0,0 +1,9 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples:
+
+// // returns 'www.codewars.com'
+// removeUrlAnchor('www.codewars.com#about')
+
+// // returns 'www.codewars.com?page=1' 
+// removeUrlAnchor('www.codewars.com?page=1')

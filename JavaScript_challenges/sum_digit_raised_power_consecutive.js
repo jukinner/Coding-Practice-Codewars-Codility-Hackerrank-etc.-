752bccae228e4ff0e8d91354a9d4c86267dda246
Justin Kinner <jkinner@gmail.com>
2018-08-03 11:20:08 -0600

    added additional best practice solution

--- JavaScript_challenges/sum_digit_raised_power_consecutive.js
+++ JavaScript_challenges/sum_digit_raised_power_consecutive.js
@@ -50,4 +50,12 @@ function sumDigPow(a, b) {
 }
 
 // best practice
-
+function sumDigPow(a, b) {
+  var ans = [];
+  while(a <= b){
+    if(a.toString().split('').reduce((x,y,i)=>x + +y ** (i + 1),0) == a)
+      ans.push(a);
+    a++;
+  }
+  return ans;
+}

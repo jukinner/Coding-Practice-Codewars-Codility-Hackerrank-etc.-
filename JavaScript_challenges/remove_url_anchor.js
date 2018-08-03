c47c988a7aeb6bbd9635cfea3e1c1b7ad76dc412
Jukinner <jukinner@gmail.com>
2018-08-03 17:30:14 -0600

    remove URL anchor

--- JavaScript_challenges/remove_url_anchor.js
+++ JavaScript_challenges/remove_url_anchor.js
@@ -7,3 +7,15 @@
 
 // // returns 'www.codewars.com?page=1' 
 // removeUrlAnchor('www.codewars.com?page=1')
+// one way
+function removeUrlAnchor(url){
+  return url.slice(0,16)
+}
+
+// 2nd way
+function removeUrlAnchor(url){
+  return url.match(/www.codewars.com/i)[0]
+}
+
+
+// removeUrlAnchor('www.codewars.com#about')

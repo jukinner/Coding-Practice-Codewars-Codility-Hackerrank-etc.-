14f4bac561f9773b134d7d476254c270853ef110
Justin Kinner <jkinner@gmail.com>
2018-08-07 12:15:12 -0600

    counting duplicates in a a string

---
+++ counting_duplicates.rb
@@ -0,0 +1,29 @@
+# Count the number of Duplicates
+# Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+# Example
+# "abcde" -> 0 # no characters repeats more than once
+# "aabbcde" -> 2 # 'a' and 'b'
+# "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (bandB)
+# "indivisibility" -> 1 # 'i' occurs six times
+# "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+# "aA11" -> 2 # 'a' and '1'
+# "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+def duplicate_count(text)
+	test_data = text.split("").map { |y| y.upcase }
+	counter, unique = 0, test_data.uniq
+	unique.each { |x| counter += 1 if test_data.count(x) > 1 }
+	return counter
+end
+
+# Test.assert_equals(duplicate_count(""), 0)
+# Test.assert_equals(duplicate_count("abcde"), 0)
+# Test.assert_equals(duplicate_count("abcdeaa"), 1)
+# Test.assert_equals(duplicate_count("abcdeaB"), 2)
+# Test.assert_equals(duplicate_count("Indivisibilities"), 2)
+
+
+# p "abcdeaa".split("").uniq
+
+duplicate_count("abcdeaB99")

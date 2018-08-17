34a94c24738ff8a0251372ff3c663fd3da418d34
Jukinner <jukinner@gmail.com>
2018-08-17 08:49:21 -0600

    Added best practices to work on

--- ROT13_Cipher.rb
+++ ROT13_Cipher.rb
@@ -19,6 +19,16 @@ def rot13(string)
 end
 
 
+def rot13(string)
+  string.tr("A-Za-z", "N-ZA-Mn-za-m")
+end
+
+def rot13(string)
+  origin = ('a'..'z').to_a.join + ('A'..'Z').to_a.join
+  cipher = ('a'..'z').to_a.rotate(13).join + ('A'..'Z').to_a.rotate(13).join
+  string.tr(origin, cipher)
+end
+
 
 
 

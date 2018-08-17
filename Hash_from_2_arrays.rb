7c066cdce2c258a57732820878b31381735aaba0
Jukinner <jukinner@gmail.com>
2018-08-17 07:54:27 -0600

    Updated with each_with_index solution

--- Hash_from_2_arrays.rb
+++ Hash_from_2_arrays.rb
@@ -14,18 +14,23 @@
 
 require 'rspec'
 
+# First answer
 def title_builder headers, data
   i = 0
-  proof = headers.each_with_object(Hash.new) do |e, hash| 
+  headers.each_with_object(Hash.new) do |e, hash| 
     hash[e]=data[i]
     i += 1
   end
-  p proof
 end
 
-arr_1 = ['title', 'description', 'rating']
-arr_2 = ['Fountainhead', 'Novel about unique perspectives', 5]
-title_builder arr_1, arr_2
+# Second option
+def title_builder headers, data
+  hash = Hash.new
+  headers.each_with_index do |header, index|
+    hash[header] = data[index]
+  end
+  hash
+end
 
 describe 'Hash builder' do
   it 'combines two arrays to generate a hash' do

fdb65b2d847f83e1259df920b556c984024f91d8
Justin Kinner <jkinner@gmail.com>
2018-09-26 14:02:15 -0600

    added additional OOP method

--- Always_sorted_ruby.rb
+++ Always_sorted_ruby.rb
@@ -11,17 +11,46 @@ require 'rspec'
 
 def position_enforcer(bucket, *args)
   bucket = bucket.sort_by { |x| x[:position] }
-  args.each { |x|
+  args.each do |x|
     x.store(:position, bucket.length + 1) if x[:position] == nil
     bucket.insert(x[:position] - 1, x)
-    }
+    end
   i = 1
-  bucket.compact.map {|x| 
+  bucket.compact.map do |x| 
     x[:position] = i
     i += 1
-  }
+  end
   bucket.compact
 end
+
+
+# Jordan's Answer
+# def position_enforcer(pre_existing_array, new_obj_to_add)
+#   return set_default_of_position_one_for_first_element(pre_existing_array, new_obj_to_add) if pre_existing_array.empty?
+#   return add_obj_to_end_of_collection(pre_existing_array, new_obj_to_add) unless new_obj_to_add[:position]
+#   add_obj_and_resort(pre_existing_array, new_obj_to_add)
+# end
+
+# def set_default_of_position_one_for_first_element(arr, obj)
+#   obj[:position] = 1
+#   arr << obj
+# end
+
+# def add_obj_to_end_of_collection(arr, obj)
+#   stack_position = arr.last[:position] + 1
+#   obj[:position] = stack_position
+#   arr << obj
+# end
+
+# def add_obj_and_resort(arr, obj)
+#   objects_with_positions_higher = arr.select { |e| e[:position] >= obj[:position] }
+#   objects_with_positions_higher.map { |e| e[:position] += 1 }
+#   objects_with_positions_higher.sort_by { |e| e[:position] }
+#   arr << obj
+#   arr.sort_by { |e| e[:position] }
+# end
+
+
 
 describe 'always sorted object' do
   it 'assigns a position of 1 for the first object added to the array' do

2a02c1e8d8b730fbc0810326d581ee5ec4407b7c
Justin Kinner <jkinner@gmail.com>
2018-11-24 19:31:23 -0500

    added additional way to reverse string

--- reverse_string.rb
+++ reverse_string.rb
@@ -1,11 +1,22 @@
 string = "the quick Brown fox jumped over the lazy dog!"
 
-array = string.chars
-i = 0
-array.length.times do 
-  pop = array.pop
-  array = array.insert(i, pop)
-  i += 1
+# array = string.chars
+# i = 0
+# array.length.times do 
+#   pop = array.pop
+#   array = array.insert(i, pop)
+#   i += 1
+# end
+
+# p array.join()
+
+def reverse_string(string)
+	string = string.split("")
+	bucket = string.each_with_object([]) do |item, arr|
+		arr.unshift(item)
+	end
+	p bucket
 end
 
-p array.join()
+reverse_string(string)
+

858e661e4c806523d528fd0c60dfc771726241ae
Justin Kinner <jkinner@gmail.com>
2018-11-24 19:45:56 -0500

    reverse words in a string while maintaining order of words

--- reverse_string.rb
+++ reverse_string.rb
@@ -10,13 +10,20 @@ string = "the quick Brown fox jumped over the lazy dog!"
 
 # p array.join()
 
-def reverse_string(string)
-	string = string.split("")
-	bucket = string.each_with_object([]) do |item, arr|
-		arr.unshift(item)
-	end
+# def reverse_string(string)
+# 	string = string.split("")
+# 	bucket = string.each_with_object([]) do |item, arr|
+# 		arr.unshift(item)
+# 	end
+# 	p bucket
+# end
+
+# reverse_string(string)
+
+def reverse_words_in_string(string)
+	string = string.split(" ")
+	bucket = string.each_with_object([]) { |item, arr| arr << (item.split("").each_with_object([]) {|x, ar| ar.unshift(x)}.join) }
 	p bucket
 end
 
-reverse_string(string)
-
+reverse_words_in_string(string)

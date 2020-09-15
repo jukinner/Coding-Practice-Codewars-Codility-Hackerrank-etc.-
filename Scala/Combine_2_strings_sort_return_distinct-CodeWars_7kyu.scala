8e82ebd7595645ca3169ec590f479d313fd24691
jkinner <jkinnera@mastercontrol.com>
2020-09-15 09:26:39 -0600

    First Scala challenge-combining 2 strings, sortWith, mkString, Distinct

---
+++ Scala/Combine_2_strings_sort_return_distinct-CodeWars_7kyu.scala
@@ -0,0 +1,38 @@
+// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
+
+// each taken only once - coming from s1 or s2.
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+//MY Answer
+object Kata {
+
+  def longest(s1: String, s2: String): String = {
+    var newStr = s1 + s2
+    newStr.split("").toList.sortWith(_ < _).distinct.mkString("")
+  }
+}
+
+// TESTS
+import org.scalatest._
+
+class KataTest extends FlatSpec with Matchers {
+
+  val tests = List(
+    (("aretheyhere", "yestheyarehere"), "aehrsty"),
+    (("loopingisfunbutdangerous", "lessdangerousthancoding"), "abcdefghilnoprstu"),
+    (("inmanylanguages", "theresapairoffunctions"), "acefghilmnoprstuy")
+  )
+
+  tests.foreach {
+    case ((s1, s2), expected) =>
+      s"longest($s1, $s2)" should s"return $expected" in {
+        Kata.longest(s1, s2) should be (expected)
+      }
+  }
+}

68927abb1e030dd1e72054ba97babf08489dadec
jkinner <jkinnera@mastercontrol.com>
2020-12-24 09:17:29 -0700

    Scala reverse string with String buffer and for each loop

--- Are_they_the_same?.js
+++
@@ -1,5 +0,0 @@
-// Given two arrays a and b write a function comp(a, b) (compSame(a, b) in Clojure) that checks whether the two arrays have the "same" elements, with the same multiplicities. "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
-
-function comp(array1, array2){
-  //your code here
-}
